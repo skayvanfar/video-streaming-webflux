f49c3c41838c5e80ba43d90964581542e70457d5
Saeed Kayvanfar <kayvanfar.sj@gmail.com>
2021-08-24 15:00:21 +0430

    first commit

---
+++ src/test/java/ir/sk/videostreaming/WebfluxVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sk.videostreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxVideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
